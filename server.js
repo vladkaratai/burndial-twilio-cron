4f03cca0e09aeb921f306115a3630944a4b84969
vladkaratai <vladislav.karatai@interexy.com>
2025-09-29 22:48:42 +0300

    daw

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ app.get('/events', (req, res) => {
   res.setHeader('Content-Type', 'text/event-stream');
   res.setHeader('Cache-Control', 'no-cache');
   res.setHeader('Connection', 'keep-alive');
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
   res.flushHeaders();
 
   subscribers.add(res);
